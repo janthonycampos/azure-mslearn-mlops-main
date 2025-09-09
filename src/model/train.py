64484fa6dc668b1ee51ffdb369827a22f556f7bb
janthonycampos <camposarteaga.j@gmail.com>
2025-09-09 11:44:13 -0500

    fix lint errors

--- src/model/train.py
+++ src/model/train.py
@@ -45,6 +45,7 @@ def split_data(df):
     ].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
+
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver='lbfgs').fit(X_train, y_train)
@@ -65,6 +66,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
